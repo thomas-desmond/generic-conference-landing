21e7cdaeea2a845f0d1c593832636ee7073d3573
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2025-03-20 09:22:36 -0700

    Integrate Tailwind CSS and update configuration; remove Welcome component and replace with HackTheSafe component. Update package dependencies accordingly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
-    }
-  })
+      enabled: true,
+    },
+  }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
